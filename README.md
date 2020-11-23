f225b8effae3c46fcb50f0c426f4992c935632c4
Mika Kaukoranta <mkaukoranta@robocorp.com>
2020-11-23 13:36:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Python robot
+
+A simple web scraper robot implemented as a Python script ([tasks.py](./tasks.py)) instead of a `.robot` file, using the `rpaframework` set of libraries.
+
+See the [robot.yaml](./robot.yaml) file for how to configure the `command` to run.
+
+> You can find the full tutorial and instructions on [Robocorp's documentation site](https://robocorp.com/docs/development-howtos/python/python-robot).
---
+++ tasks.py
@@ -0,0 +1,35 @@
+from RPA.Browser import Browser
+
+browser = Browser()
+url = "https://robocorp.com/docs/"
+term = "python"
+screenshot_filename = "output/screenshot.png"
+
+
+def open_the_website(url: str):
+    browser.open_available_browser(url)
+
+
+def search_for(term: str):
+    input_field = "css:input"
+    browser.input_text(input_field, term)
+    browser.press_keys(input_field, "ENTER")
+
+
+def store_screenshot(filename: str):
+    browser.screenshot(filename=filename)
+
+
+# Define a main() function that calls the other functions in order:
+def main():
+    try:
+        open_the_website(url)
+        search_for(term)
+        store_screenshot(screenshot_filename)
+    finally:
+        browser.close_all_browsers()
+
+
+# Call the main() function, checking that we are running as a stand-alone script:
+if __name__ == "__main__":
+    main()
