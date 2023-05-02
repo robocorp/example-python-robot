478d388787132dd4045532636c55ff3093b60e0b
Ayrton Denner <ayrtondenner_2013@hotmail.com>
2023-05-02 20:32:15 -0300

    Finished extracting data from website

--- default_variables.py
+++ default_variables.py
@@ -2,5 +2,14 @@ WEBSITE = "https://www.nytimes.com/"
 WEBSITE_QUERY = "https://www.nytimes.com/search?query={query}"
 
 SEARCH_PHRASE = "Economy"
-NEWS_CATEGORY_OR_SECTION = ["Business", "Opinion", "U.S.", "Week in Review", "World"]
-MONTHS = 3
+NEWS_CATEGORY_OR_SECTION = ["Test A", "Business", "Test B", "Opinion", "U.S.", "Week in Review", "World"]
+MONTHS = 1
+
+CURRENCY_REGEX_LIST = [
+    "\$\d{1,3}\.\d{1,2}",           # $11.1
+    "\$\d{1,3}\,\d{3}\.\d{1,2}",    # $111,111.11
+    "\d{1,3} dollars",              # 11 dollars
+    "\d{1,3} usd"                   # 11 USD
+]
+
+CURRENCY_REGEX_LIST_JOINED = '(?:%s)' % '|'.join(CURRENCY_REGEX_LIST)
--- tasks.py
+++ tasks.py
@@ -5,6 +5,9 @@ from RPA.Robocorp.Vault import Vault
 import default_variables
 from helpers import date_helper
 
+from datetime import datetime
+import time, re
+
 browser_lib = Selenium()
 
 def get_work_variables():
@@ -65,19 +68,191 @@ def access_search_page(query, website_query):
         website_query_url = website_query.format(query = query)
         browser_lib.go_to(website_query_url)
 
+def close_cookies_windows():
+    try:
+        browser_lib.click_button("xpath://button[@data-testid='expanded-dock-btn-selector']")
+    except:
+        printing_message("Error when closing cookies window")
 
-def apply_filters(news_category, months):
-    # Applying months filter
 
-    # Fixing months if something happens
-    months = date_helper.fixing_months_variable(months)
-    start_date_string, end_date_string = date_helper.calculate_start_and_end_date(months)
+def apply_date_range_filter(months):
+    try:
+        # Applying months filter
+
+        # Fixing months if something happens
+        months = date_helper.fixing_months_variable(months)
+        start_date_string, end_date_string = date_helper.calculate_start_and_end_date(months)
+
+        date_range_selector = browser_lib.find_element("xpath://button[@data-testid='search-date-dropdown-a']")
+        date_range_selector.click()
+        
+        # Unfortunately there's no id for this button or any parent element
+        # So we had to go after child button via "value" property
+        browser_lib.click_button("xpath://div[@aria-label='Date Range']//button[@value='Specific Dates']")
+        
+        browser_lib.input_text("startDate", start_date_string)
+        browser_lib.input_text("endDate", end_date_string)
+
+        # Clicking again to dismiss the dialog
+        date_range_selector.click()
+    except Exception as ex:
+        printing_message("Error when applying date range filter")
+
+def apply_section_filter(news_category_list):
+    try:
+        # If there's no category to select, we can let the filter as it is
+        # Which is the "Any" category
+        if len(news_category_list) == 0:
+            pass
+
+        section_selector = browser_lib.find_element("xpath://button[@data-testid='search-multiselect-button']")
+        section_selector.click()
+
+        for news_category in news_category_list:
+            try:
+                section_search_count = browser_lib.get_element_count(f"//span[text()='{news_category}']")
+
+                if section_search_count == 1:
+                    browser_lib.click_element(f"//span[text()='{news_category}']")
+                else:
+                    printing_message(f"Section '{news_category}' not found")
+            except Exception as ex:
+                printing_message(f"Error when trying to select '{news_category}' filter section")
+
+        # Clicking again to dismiss the dialog
+        section_selector.click()
+        #a = 5 / 0
+    except Exception as ex:
+        printing_message("Error when applying section filter")
+
+        # If any major error happens
+        # we will go back to selecting "Any" section
+        select_section_any()
+
+
+def select_section_any():
+    try:
+        section_button = browser_lib.find_element(f"xpath://button[@data-testid='search-multiselect-button']")
+
+        section_button_class = section_button.get_attribute("class")
+
+        # This means that the Section dialog is not open
+        if not "popup-visible" in section_button_class:
+            section_button.click()
+
+        browser_lib.click_element(f"//span[text()='Any']")
+
+        # Clicking again to dismiss the dialog
+        section_button.click()
+
+    except:
+        printing_message("Error when applying 'Any' section filter")
+
+def select_sort_by_newest():
+    try:
+        sort_selector = browser_lib.find_element("xpath://select[@data-testid='SearchForm-sortBy']")
+
+        sort_selector.click()
+        browser_lib.click_element("xpath://select[@data-testid='SearchForm-sortBy']/option[@value='newest']")
+        
+        # Clicking again to dismiss the dialog
+        sort_selector.click()
+    except:
+        printing_message("Error when sorting by newest")
+
+# In best case scenario, isn't needed to press this button
+# since the sort update will already update the query result
+# But if anything wrong happens, this click will ensure that the search will happen anyway
+def press_search_page_button():
+    browser_lib.click_button("xpath://button[@data-testid='search-page-submit']")
+
+def extract_articles(query):
+    scroll_all_articles()
+    article_info_list = get_articles_info(query)
+
+    return article_info_list
+
+def scroll_all_articles():
+    show_more_button_xpath = "xpath://button[@data-testid='search-show-more-button']"
+    articles_xpath = "xpath://ol[@data-testid='search-results']//li[@data-testid='search-bodega-result']"
+    articles_count = 0
     
-    pass
+    while browser_lib.get_element_count(show_more_button_xpath) == 1:
+        try:
+            # Sometimes, it takes some time for new articles to load
+            # if that happens, it's better to wait them to load
+            # instead of clicking the "show more" button again
+            # Also, during the last batch of articles
+            # the button is still visible until the articles are loaded
+            # and only after that the button disappear
+            # Because of that, it is needed to check if the article list changed
+            # before trying to get the "show more" button again
+            if articles_count > 0 and browser_lib.get_element_count(articles_xpath) == articles_count:
+                #time.sleep(3)
+                continue
 
+            articles_count = browser_lib.get_element_count(articles_xpath)
+
+            browser_lib.scroll_element_into_view(show_more_button_xpath)
+            browser_lib.click_button_when_visible(show_more_button_xpath)
+        except Exception as ex:
+            break
+        
+
+def get_articles_info(query):
+    article_info_list = []
+
+    articles_xpath = "xpath://ol[@data-testid='search-results']//li[@data-testid='search-bodega-result']"
+    articles_count = browser_lib.get_element_count(articles_xpath)
+
+    query_lower = query.lower()
+
+    # Using full chained xpath
+    # because using element.find_elements to find children
+    # only throws exception
+    for i in range(1, articles_count + 1):
+        try:
+            article_children_div_xpath = f"{articles_xpath}[{i}]/div/"
+
+            title = get_attribute_inner_html            (f"{article_children_div_xpath}div/div/a/h4")
+            date = get_attribute_inner_html             (f"{article_children_div_xpath}span")
+            description = get_attribute_inner_html      (f"{article_children_div_xpath}div/div/a/p[1]")
+            picture_filename = browser_lib.find_element (f"{article_children_div_xpath}div/figure/div/img").get_attribute("src")
+            
+            title_lower = title.lower()
+            description_lower = description.lower()
+
+            search_phrases_count = title_lower.count(query_lower) + description_lower.count(query_lower)
+            contains_any_amount_of_money = (
+                len(re.findall(default_variables.CURRENCY_REGEX_LIST_JOINED, title_lower)) > 0
+                or
+                len(re.findall(default_variables.CURRENCY_REGEX_LIST_JOINED, description_lower)) > 0
+            )
+
+            article_dict = {
+                "title": title,
+                "date": date,
+                "description": description,
+                "picture_filename": picture_filename,
+                "search_phrases_count": search_phrases_count,
+                "contains_any_amount_of_money": contains_any_amount_of_money
+            }
+
+            article_info_list.append(article_dict)
+
+        except Exception as ex:
+            printing_message(ex)
+
+    return article_info_list
 
 def store_screenshot(filename):
     browser_lib.screenshot(filename=filename)
+
+def printing_message(message):
+    print(f"{str(datetime.now())} - {message}")
+
+def get_attribute_inner_html(element_path):
+    return browser_lib.find_element(element_path).get_attribute("innerHTML")
 
 
 # Define a main() function that calls the other functions in order:
@@ -86,10 +261,14 @@ def main():
         variables = get_work_variables()
         open_the_website(variables["website"])
         access_search_page(variables["search_phrase"], variables["website_query"])
-        apply_filters(variables["news_category_or_section"], variables["months"])
-        store_screenshot("output/screenshot.png")
+        close_cookies_windows()
+        apply_date_range_filter(variables["months"])
+        apply_section_filter(variables["news_category_or_section"])
+        select_sort_by_newest()
+        press_search_page_button()
+        article_info_list = extract_articles(variables["search_phrase"])
     except Exception as ex:
-        print(ex)
+        printing_message(ex)
     finally:
         browser_lib.close_all_browsers()
 
